c0cdc4370368dd0a1f62442a4c1af3ff53b69f01
Pavel <shchp@vk.com>
2022-06-21 10:42:34 +0300

    HomeWork №12 (Second)

--- HomeWork_12/Task7.py
+++ HomeWork_12/Task7.py
@@ -1,13 +1,30 @@
 #Знать бы ещё формулу подбора корней кубического уравнения, программа может и быстрее работала бы =)
+# def height(save):
+#     depth = 0
+#     can = -1000
+#     while depth < 3 :
+#         if save == depth ** 3 - 3 * (depth ** 2) - 12 * depth + 10:
+#             can = depth
+#         depth += 0.000000001
+#         print(depth, can)
+#     return can
+#
+# print(height(0.01))
 
-def height(save):
-    depth = 0
-    can = -1000
-    while depth < 3 :
-        if save == depth ** 3 - 3 * (depth ** 2) - 12 * depth + 10:
-            can = depth
-        depth += 0.000000001
-        print(depth, can)
-    return can
+d_from = 0
+d_to = 4
+max_danger = float(input("Введите максимально допустимый уровень опасности: "))
+depth = d_from + (d_to - d_from) / 2
+danger = depth ** 3 - 3 * depth ** 2 - 12 * depth + 10
+if max_danger < 0:
+    print("Ошибка")
+else:
+    print(f"Depth: {depth}  Danger: {danger}")
+    while abs(danger) > max_danger:
+        if danger > 0:
+            d_from = depth
+        else:
+            d_to = depth
 
-print(height(0.01))
+        depth = d_from + (d_to - d_from) / 2
+        danger = depth ** 3 - 3 * depth ** 2 - 12 * depth + 10
--- HomeWork_12/Task9.py
+++ HomeWork_12/Task9.py
@@ -1,5 +1,3 @@
-# Не понял ни задание, ни предоставленную формулу...
-
 credit = input("Введите сумму кредита ")
 credit = credit.split("e")
 credit = int(int(credit [0]) * 10 ** int(credit[1]))
@@ -7,7 +5,51 @@ credit = int(int(credit [0]) * 10 ** int(credit[1]))
 year = int(input("На сколько лет выдан кредит? "))
 procent = float(input("Сколько процентов годовых? "))
 procent /= 100
-K = (procent * ((1 + procent) ** year)) / (1 + procent) ** year - 1
-A = credit * K
-print(K)
-print(A)
+tr = True
+for i in range(1,4):
+
+    K = (procent * ((1 + procent) ** year)) / (1 + procent) ** year
+    A = credit * K
+    site_K = (procent) * ((1 + procent) ** year) / ((1 + procent) ** year - 1)
+    print(f"Период {i}")
+    print(f"Остаток долга: {credit}")
+    print("K - ", K)
+    print(f"Выплачено процентов {A}")
+    if tr == True:
+        proc = credit * site_K
+        print("proc - ", proc)
+        time = proc
+        tr = False
+    body = proc - A
+    print(f"Выплачено тела кредита : {body}")
+    credit -= body
+    print()
+print(f"Остаток долга - {credit}")
+print("\n\n====================\n\n")
+
+i = 4
+
+year = year - 3 + int(input("На сколько лет продлевается договор?"))
+procent = float(input("Увеличение ставки до "))
+procent /= 100
+tr = True
+while credit > 1:
+    K = (procent * ((1 + procent) ** year)) / (1 + procent) ** year
+    A = credit * K
+    site_K = (procent) * ((1 + procent) ** year) / ((1 + procent) ** year - 1)
+    print(f"Период {i}")
+    print(f"Остаток долга: {credit}")
+    print("K - ", K)
+    print(f"Выплачено процентов {A}")
+    if tr == True:
+        proc = credit * site_K
+        print("proc - ", proc)
+        time = proc
+        tr = False
+    body = proc - A
+    print(f"Выплачено тела кредита : {body}")
+    credit -= body
+    print()
+    i += 1
+
+print(f"Осталось - {credit}")
